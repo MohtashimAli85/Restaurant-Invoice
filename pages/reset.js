f3c56ab5e3828e683dba1d2457b2cdbe2f237dc7
Mohtashim Ali <mohtashima85@gmail.com>
2021-09-26 21:53:47 +0500

    Update reset.js

--- pages/reset.js
+++ pages/reset.js
@@ -1,4 +1,3 @@
-
 export const takeAway = document.querySelector(".takeAway");
 export const categories = document.querySelector(".categories");
 export const orderSelection = document.querySelector(".orderSelection");
@@ -9,59 +8,58 @@ export let orderNowBtn = document.querySelector(".orderNow");
 export const assignTables = document.querySelector(".tables");
 export const car = document.querySelector(".car");
 export let backBtnArr = [
-    {
-        vname: categories,
-        value: "block",
-        command: "add",
-        class: "animation"
-    },
-    {
-        vname: orderSelection,
-        value: "none",
-        command: "add",
-        class: "animation"
-    },
-    {
-        vname: reserveTableBtn,
-        value: "none",
-        command: "remove",
-        class: "reserveActive"
-    },
-    {
-        vname: orderNowBtn,
-        value: "block",
-        command: "",
-        class: ""
-    },
-    {
-        vname: tableOrder,
-        value: "none",
-        command: "",
-        class: ""
-    },
-    {
-        vname: carOrder,
-        value: "none",
-        command: "",
-        class: ""
-    },
-    {
-        vname: car,
-        value: "",
-        command: "remove",
-        class: "active"
-    },
-    {
-        vname: takeAway,
-        value: "",
-        command: "remove",
-        class: "active"
-    },
-    {
-        vname: assignTables,
-        value: "",
-        command: "remove",
-        class: "active"
-    }
+  {
+    vname: categories,
+    value: "block",
+    command: "add",
+    class: "animation",
+  },
+  {
+    vname: orderSelection,
+    value: "none",
+    command: "add",
+    class: "animation",
+  },
+  {
+    vname: reserveTableBtn,
+    value: "none",
+    command: "remove",
+    class: "reserveActive",
+  },
+  {
+    vname: orderNowBtn,
+    value: "block",
+    command: "",
+    class: "",
+  },
+  {
+    vname: tableOrder,
+    value: "none",
+    command: "",
+    class: "",
+  },
+  {
+    vname: carOrder,
+    value: "none",
+    command: "",
+    class: "",
+  },
+  {
+    vname: car,
+    value: "",
+    command: "remove",
+    class: "active",
+  },
+  {
+    vname: takeAway,
+    value: "",
+    command: "remove",
+    class: "active",
+  },
+  {
+    vname: assignTables,
+    value: "",
+    command: "remove",
+    class: "active",
+  },
 ];
-
