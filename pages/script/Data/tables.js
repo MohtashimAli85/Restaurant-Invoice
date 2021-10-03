b73882880ea0cbb7d8673914b11a759bc856516b
Mohtashim Ali <mohtashima85@gmail.com>
2021-10-03 21:33:46 +0500

    Update tables.js

--- pages/script/Data/tables.js
+++ pages/script/Data/tables.js
@@ -1,74 +1,76 @@
-export let tables = [
-  {
-    tableNo: 1,
-    reserved: false,
-  },
-  {
-    tableNo: 2,
-    reserved: false,
-  },
-  {
-    tableNo: 3,
-    reserved: false,
-  },
-  {
-    tableNo: 4,
-    reserved: false,
-  },
-  {
-    tableNo: 5,
-    reserved: false,
-  },
-  {
-    tableNo: 6,
-    reserved: false,
-  },
-  {
-    tableNo: 7,
-    reserved: false,
-  },
-  {
-    tableNo: 8,
-    reserved: false,
-  },
-  {
-    tableNo: 9,
-    reserved: false,
-  },
-  {
-    tableNo: 10,
-    reserved: false,
-  },
-  {
-    tableNo: 11,
-    reserved: false,
-  },
-  {
-    tableNo: 12,
-    reserved: false,
-  },
-  {
-    tableNo: 13,
-    reserved: false,
-  },
-  {
-    tableNo: 14,
-    reserved: false,
-  },
-  {
-    tableNo: 15,
-    reserved: false,
-  },
-  {
-    tableNo: 16,
-    reserved: false,
-  },
-  {
-    tableNo: 17,
-    reserved: false,
-  },
-  {
-    tableNo: 18,
-    reserved: false,
-  },
-];
+export let tables = localStorage.getItem('tables')
+  ? JSON.parse(localStorage.getItem('tables'))
+  : [
+    {
+      tableNo: 1,
+      reserved: false,
+    },
+    {
+      tableNo: 2,
+      reserved: false,
+    },
+    {
+      tableNo: 3,
+      reserved: false,
+    },
+    {
+      tableNo: 4,
+      reserved: false,
+    },
+    {
+      tableNo: 5,
+      reserved: false,
+    },
+    {
+      tableNo: 6,
+      reserved: false,
+    },
+    {
+      tableNo: 7,
+      reserved: false,
+    },
+    {
+      tableNo: 8,
+      reserved: false,
+    },
+    {
+      tableNo: 9,
+      reserved: false,
+    },
+    {
+      tableNo: 10,
+      reserved: false,
+    },
+    {
+      tableNo: 11,
+      reserved: false,
+    },
+    {
+      tableNo: 12,
+      reserved: false,
+    },
+    {
+      tableNo: 13,
+      reserved: false,
+    },
+    {
+      tableNo: 14,
+      reserved: false,
+    },
+    {
+      tableNo: 15,
+      reserved: false,
+    },
+    {
+      tableNo: 16,
+      reserved: false,
+    },
+    {
+      tableNo: 17,
+      reserved: false,
+    },
+    {
+      tableNo: 18,
+      reserved: false,
+    },
+  ];
