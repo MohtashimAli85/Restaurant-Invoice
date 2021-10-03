1e3561eb2d10289336ee4d92e5fd77188fb0422d
Mohtashim Ali <mohtashima85@gmail.com>
2021-10-03 13:13:04 +0500

    Update menu.js

--- pages/script/Menu/menu.js
+++ pages/script/Menu/menu.js
@@ -1,67 +1,30 @@
-import { muttonMenu } from "../Data/mutton.js";
-import { chickenMenu } from "../Data/chicken.js";
-import { sideOrderMenu } from "../Data/sideOrder.js";
+import { fillDataArr } from "../Data/fillData.js";
+import { menuListenerArr } from "../Data/eventListeners.js";
 import { validateActive, menuFn, fillingData } from "../functions/function.js";
-// import { } from "../test.js";
-const menuOne = document.querySelector(".menu1");
-const menuTwo = document.querySelector(".menu2");
-const menuThree = document.querySelector(".menu3");
-const menuFour = document.querySelector(".menu4");
 
-const mutton = document.getElementById("mutton");
-const chicken = document.getElementById("chicken");
-const drinks = document.getElementById("drinks");
-const sideOrder = document.getElementById("sides");
-
-let item = "";
 let orderNowBtn = document.querySelector(".orderNow");
-let items = document.querySelector(".items");
-let fillDataArr = [
-    {
-        Vname: muttonMenu,
-        item: item,
-        img: "rack-of-lamb",
-        imgName: "mutton",
-        menu: mutton
-    }
-];
-fillingData(muttonMenu, item, "rack-of-lamb", "mutton", mutton);
-// muttonMenu.forEach((e) => {
-//     item += dataProvider(e.name, e.type, e.price, "rack-of-lamb", "mutton");
-// });
-// mutton.innerHTML = item;
-// item = "";
-// chickenMenu.forEach((e) => {
-//     item += dataProvider(e.name, e.type, e.price, "chicken-leg", "chicken");
+let items = document.querySelector(".items").innerHTML;
+let args = [];
 
-// });
-// chicken.innerHTML = item;
-// item = "";
-
-// sideOrderMenu.forEach((e) => {
-//     item += dataProvider(e.name, e.type, e.price, "menu", "sideOrder")
-// });
-// sideOrder.innerHTML = item;
-// item = "";
-
-menuOne.addEventListener("click", () => {
-    menuFn(menuTwo, "#menu2", menuThree, "#menu3", menuFour, "#menu4", menuOne, "#menu1"
-    );
-});
-menuTwo.addEventListener("click", () => {
-    menuFn(menuOne, "#menu1", menuThree, "#menu3", menuFour, "#menu4", menuTwo, "#menu2");
-});
-menuThree.addEventListener("click", () => {
-    menuFn(menuOne, "#menu1", menuTwo, "#menu2", menuFour, "#menu4", menuThree, "#menu3");
+fillDataArr.forEach(e => {
+    fillingData(e.Vname, e.item, e.img, e.imgName, e.menu);
+    e.Vname.innerHTML = e.item;
+    e.item = "";
 });
 
-menuFour.addEventListener("click", () => {
-    menuFn(menuOne, "#menu1", menuTwo, "#menu2", menuThree, "#menu3", menuFour, "#menu4");
+menuListenerArr.forEach(e => {
+    e.Vname.addEventListener("click", () => {
+        menuFn(e.args[0], e.args[1], e.args[2], e.args[3], e.args[4], e.args[5], e.args[6], e.args[7]);
+    });
 });
+
+menuListenerArr.forEach(e => {
+    args.push(e.Vname);
+});
+
 orderNowBtn.addEventListener("click", () => {
-    if (items.innerHTML != "0") {
-        validateActive(menuOne, menuTwo, menuThree, menuFour);
+    if (items != "0") {
+        validateActive(args[0], args[1], args[2], args[3]);
     }
 });
-
 export let addBtn = document.querySelectorAll('.addBtn');
