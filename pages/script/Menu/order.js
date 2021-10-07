13e068eecaade3611c1241dcf31fa6001b933c61
Mohtashim Ali <mohtashima85@gmail.com>
2021-10-07 12:34:12 +0500

    Update order.js

--- pages/script/Menu/order.js
+++ pages/script/Menu/order.js
@@ -1,7 +1,7 @@
 import { footer, orderNowBtnArr, orderNowBtn, takeAway } from "../Component/reset.js";
-import { updatePrice, display } from "../functions/function.js";
+import { updatePrice, display, getOrderItem, pdCal, delButtons, qtyListener } from "../functions/function.js";
 import { addBtn } from "./menu.js";
-
+// import { setTableClick } from "../../../script/index.js"
 export let bill = document.querySelector(".bill");
 export let totals = document.querySelectorAll(".total");
 
@@ -17,27 +17,91 @@ let delBtns = document.querySelectorAll(".delImg"),
     ? JSON.parse(localStorage.getItem('takeAway'))
     : [],
   item = "", name = "", price = "", qty = "", newItem = "", tBill = 0;
+let tableOrder = localStorage.getItem('tableOrder') ?
+  JSON.parse(localStorage.getItem('tableOrder')) : [],
+  tableClick = sessionStorage.getItem("tableClick") ?
+    JSON.parse(sessionStorage.getItem("tableClick")) : "";
+let tableOrderItem = [];
+if (tableOrder) {
+  tableOrder.forEach(e => {
+    tableOrderItem.push(e.name);
+  });
+}
+if (tableClick.tableClicked) {
+  item = "";
+  tableOrder = localStorage.getItem('tableOrder') ?
+    JSON.parse(localStorage.getItem('tableOrder')) : [];
+  let tableOrderItem = [];
+  if (tableOrder) {
+    tableOrder.forEach(e => {
+      e.description.forEach(e => {
+        tableOrderItem.push(e);
+      });
+    });
+  }
+  tableOrderItem.forEach(e => {
+    item += `<div class="orderItem orderItemAnimation">
+        <div class="orderName d-flex">
+            <h5>${e.itemName}</h5>
+            <img src="../assets/deleteIcon.svg" alt="delete icon" class="delImg">
+        </div>
+        <div class="orderPrice d-flex">
+            <div class="count d-flex">
+                <p class="qty new">${e.qty}</p>
+            </div>
+            <div class="calculation d-flex">
+                <p class="price">${e.price}</p>
+                <p class="total">${e.total}</p>
+            </div>
+        </div>
+    </div>`;
+  });
+
+  orderContainer.innerHTML = item;
+  display(footer, "40%", "flexBasis", "block");
+  main.style.flexBasis = "60%";
+  qty = document.querySelector(".qty");
+  newItem = document.querySelector(".new");
+  delBtns = document.querySelectorAll(".delImg");
+  totals = document.querySelectorAll(".total");
+  orderItem = document.querySelectorAll(".orderItem");
+  let pastEdit = document.querySelector(".edit");
+  if (newItem) {
+    newItem.classList.add("edit");
+    newItem.classList.remove("new");
+    if (pastEdit != null) {
+      pastEdit.classList.remove("edit");
+    }
+  }
+
+  item = "";
+  delButtons(delBtns, totals, qty, items, tBill, bill, footer, main);
+
+  qty = document.querySelectorAll(".qty");
+  qtyListener(qty, pastEdit);
+
+  items.innerHTML = qty.length;
+  tBill = 0;
+  totals.forEach((e) => {
+    tBill += Number(e.innerHTML);
+  });
+  bill.innerHTML = tBill;
+}
 if (document.querySelector(".menu1")) {
   document.querySelector(".menu1").click();
 }
-
 addBtn.forEach((e) => {
-
   e.addEventListener("click", (e) => {
     display(footer, "40%", "flexBasis", "block");
     main.style.flexBasis = "60%";
-
     item = e.target.previousElementSibling;
     name = item.children[1].children[0].innerHTML;
     price = item.children[1].children[1].innerHTML;
     price = price.split(".");
-    if (name.includes("PD")) {
-      price[1] = Math.round(Number(price[1]) / 12);
-    }
+    price[1] = pdCal(name, price[1]);
     orderItem = document.querySelectorAll(".orderItem");
     let match = false;
     if (orderItem.length > 0) {
-      console.log();
       orderItem.forEach(e => {
         if (name == e.children[0].children[0].innerHTML) {
           let qty = e.children[1].children[0].children[0];
@@ -46,8 +110,8 @@ addBtn.forEach((e) => {
           updatePrice(qty);
         }
       });
-
-    } if (!match) {
+    }
+    if (!match) {
       item = `<div class="orderItem orderItemAnimation">
         <div class="orderName d-flex">
             <h5>${name}</h5>
@@ -84,51 +148,16 @@ addBtn.forEach((e) => {
         pastEdit.classList.remove("edit");
       }
     }
-
     item = "";
-    delBtns.forEach((e) => {
-      e.addEventListener("click", (e) => {
-        e.target.parentNode.parentNode.classList.add("orderItemAnimationR");
-        setTimeout(() => {
-          e.target.parentNode.parentNode.remove();
-          totals = document.querySelectorAll(".total");
-          qty = document.querySelectorAll(".qty");
-          let count = 0;
-          qty.forEach((e) => {
-            count += Number(e.innerHTML);
-          });
-          items.innerHTML = count;
-          tBill = 0;
-          totals.forEach((e) => {
-            tBill += Number(e.innerHTML);
-          });
-          bill.innerHTML = tBill;
-          if (tBill == 0) {
-            display(footer, "0%", "flexBasis", "none");
-            main.style.flexBasis = "100%";
-          }
-        }, 500);
-      });
-    });
-
+    delButtons(delBtns, totals, qty, items, tBill, bill, footer, main);
     qty = document.querySelectorAll(".qty");
-    qty.forEach((e) => {
-      e.addEventListener("click", () => {
-        pastEdit = document.querySelector(".edit");
-        if (pastEdit != null) pastEdit.classList.remove("edit");
-        e.classList.add("edit");
-      });
-    });
+    qtyListener(qty, pastEdit);
     items.innerHTML = qty.length;
     tBill = 0;
     totals.forEach((e) => {
       tBill += Number(e.innerHTML);
     });
     bill.innerHTML = tBill;
-    if (!tBill) {
-      menuGrid = document.querySelector(".menuGrid");
-      menuGrid.classList.remove("col-3");
-    }
   });
 
 });
@@ -152,23 +181,16 @@ orderNowBtn.addEventListener("click", () => {
     body.classList.add("animation");
     orderContainer.style.height = "66vh";
     if (takeAway.classList.contains("active")) {
-      let name = "";
       orderItem = document.querySelectorAll(".orderItem");
-      orderItem.forEach((e) => {
-        name += `${e.children[0].children[0].innerHTML} ${e.children[1].children[0].children[0].innerHTML}, `;
-      });
-      name = name.slice(0, -2);
+      getOrderItem(orderItem, "takeAway");
       orderArray.push({
         description: name,
         amount: Number(bill.innerHTML),
       });
       localStorage.setItem("takeAway", JSON.stringify(orderArray));
-      console.log(localStorage.getItem("takeAway"));
     }
   }
 });
-
-
 let array = [];
 let itemNames = "";
 let itemPrice = "";
