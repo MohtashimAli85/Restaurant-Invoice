b62f94e94030a29db3640273e49b939e38a2dd1d
Mohtashim Ali <mohtashima85@gmail.com>
2021-10-18 23:41:20 +0500

    Update index.js

--- script/index.js
+++ script/index.js
@@ -10,12 +10,15 @@ let orders = document.querySelector("#orders"),
     reservedList = document.querySelector(".reservedList"),
     reservedTable = document.querySelectorAll(".reservedTable"),
     modal = document.querySelector(".modal"),
+    modalw = document.querySelector(".modal-wrap"),
     updateBtn = document.querySelector(".updateBtn"),
-    printBtn = document.querySelector(".printBtn"),
+    invoiceBtn = document.querySelector(".invoiceBtn"),
     closeBtn = document.querySelector(".closeBtn"),
+    backBtn = document.querySelector(".backBtn"),
     invoice = document.querySelector(".invoice"),
     billInvoice = document.querySelector(".billInvoice"),
-    totalInvoice = document.querySelector(".totalInvoice");
+    totalInvoice = document.querySelector(".totalInvoice"),
+    salesTable = document.querySelector(".main");
 let rows = "", item = "", tableClick = false;
 let orderId = 1;
 console.log("🚀 ~ file: index.js ~ line 3 ~ orderArray", orderArray);
@@ -32,10 +35,6 @@ if (orderArray.length != 0) {
         orderId++;
     })
     orders.innerHTML = rows;
-    // console.log(orderArray);
-} else {
-    // console.log(orderArray + " else");
-
 }
 let amount = document.querySelectorAll('.amount');
 let total = document.querySelector('.total');
@@ -43,7 +42,6 @@ let totalAmount = 0;
 amount.forEach(e => {
     totalAmount += Number(e.innerHTML);
 });
-// console.log
 if (totalAmount) {
     total.innerHTML = totalAmount;
 }
@@ -68,28 +66,29 @@ reservedTable.forEach(e => {
             tableNo: tableNum
         }
         modal.style.display = 'flex';
+        modalw.classList.add("pressedBtn");
         console.log(tableClick);
         sessionStorage.setItem("tableClick", JSON.stringify(tableClick));
-
-        // window.location.href = "pages/order.html";
-        // sessionStorage.setItem("tableClick", JSON.stringify(tableClick));
     });
 });
 
 closeBtn.addEventListener("click", () => {
     tableClick = "";
     sessionStorage.setItem("tableClick", "");
+    modalw.classList.remove("pressedBtn");
     modal.style.display = "none";
+    salesTable.style.display = "block";
+
 });
 
 updateBtn.addEventListener("click", () => {
     // console.log(tableClick);
     window.location.href = "pages/order.html";
 });
-printBtn.addEventListener("click", () => {
-    let tableOrderItem = [];
+invoiceBtn.addEventListener("click", () => {
+    sessionStorage.setItem("tableClick", "");
+    let tableOrderItem = [], serviceTax = 0;
     billInvoice.style.display = "block";
-
     item = "";
     if (tableOrder) {
         tableOrder.forEach(e => {
@@ -101,6 +100,7 @@ printBtn.addEventListener("click", () => {
         });
     }
     if (tableOrderItem) {
+        salesTable.style.display = "none";
         totalAmount = 0;
         tableOrderItem.forEach(e => {
             let price = e.price;
@@ -110,14 +110,27 @@ printBtn.addEventListener("click", () => {
                         <td>${e.qty}</td>
                         <td>${price}</td>
                         <td>${e.total}</td>
-                    </tr>`
+                    </tr>`;
             totalAmount += Number(e.total);
         });
+        serviceTax = Math.round(totalAmount * 0.03);
+        item += `<tr>
+                        <td colspan="3">Service Tax</td>
+                        
+                        <td>${serviceTax}</td>
+                </tr>`;
+        item += `<tr>
+                    <td colspan="3">Received Cash</td>
+                    <td class="cashrcvd"></td>
+                </tr>`;
+        totalAmount += serviceTax;
         invoice.innerHTML = item;
         totalInvoice.innerHTML = totalAmount;
+        modal.style.display = "none";
     }
-
-
-
+});
+backBtn.addEventListener("click", () => {
+    billInvoice.style.display = "none";
+    salesTable.style.display = "block";
 
 })
