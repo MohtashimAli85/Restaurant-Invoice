b510f76d84de3e4902d41d25827119143d746f03
Mohtashim Ali <mohtashima85@gmail.com>
2021-10-23 15:03:06 +0500

    putting all files in Order folder
    Organizing files

---
+++ pages/Order/script/Menu/order.js
@@ -0,0 +1,225 @@
+import { footer, orderNowBtnArr, orderNowBtn, takeAway, car } from "../Component/reset.js";
+import { updatePrice, display, getOrderItem, pdCal, delButtons, qtyListener } from "../functions/function.js";
+import { addBtn } from "./menu.js";
+// import { setTableClick } from "../../../script/index.js"
+export let bill = document.querySelector(".bill");
+export let totals = document.querySelectorAll(".total");
+
+const orderContainer = document.querySelector(".orderContainer");
+let delBtns = document.querySelectorAll(".delImg"),
+  cancelBtn = document.querySelector(".cancel"),
+  orderItem = document.querySelectorAll(".orderItem"),
+  items = document.querySelector(".items"),
+  main = document.querySelector("main"),
+  header = document.querySelector("header"),
+  body = document.querySelector("body"),
+  menuIcon = document.querySelector(".restaurantMenu"),
+  menuGrid = "",
+  orderArray = localStorage.getItem('takeAway')
+    ? JSON.parse(localStorage.getItem('takeAway'))
+    : [],
+  item = "", name = "", price = "", qty = "", newItem = "", tBill = 0;
+let tableOrder = localStorage.getItem('tableOrder') ?
+  JSON.parse(localStorage.getItem('tableOrder')) : [],
+  tableClick = sessionStorage.getItem("tableClick") ?
+    JSON.parse(sessionStorage.getItem("tableClick")) : "",
+  updateClicked = sessionStorage.getItem("updateClick") ?
+    JSON.parse(sessionStorage.getItem("updateClick")) : "";
+let tableOrderItem = [];
+if (tableOrder) {
+  tableOrder.forEach(e => {
+    tableOrderItem.push(e.name);
+  });
+}
+if (tableClick.tableClicked) {
+  item = "";
+  tableOrder = localStorage.getItem('tableOrder') ?
+    JSON.parse(localStorage.getItem('tableOrder')) : [];
+  let tableOrderItem = [];
+  if (tableOrder) {
+    tableOrder.forEach(e => {
+      if (e.tableNum == tableClick.tableNo) {
+        e.description.forEach(e => {
+          tableOrderItem.push(e);
+        });
+      }
+    });
+  }
+  tableOrderItem.forEach(e => {
+    item += `<div class="orderItem orderItemAnimation">
+        <div class="orderName d-flex">
+            <h5>${e.itemName}</h5>
+            <img src="../../assets/deleteIcon.svg" alt="delete icon" class="delImg">
+        </div>
+        <div class="orderPrice d-flex">
+            <div class="count d-flex">
+                <p class="qty new">${e.qty}</p>
+            </div>
+            <div class="calculation d-flex">
+                <p class="price">${e.price}</p>
+                <p class="total">${e.total}</p>
+            </div>
+        </div>
+    </div>`;
+  });
+
+  orderContainer.innerHTML = item;
+  setTimeout(() => {
+    changeLayout(1, "40%", "block", "0%", "none", "60%");
+
+  }, 1500)
+
+  qty = document.querySelector(".qty");
+  newItem = document.querySelector(".new");
+  delBtns = document.querySelectorAll(".delImg");
+  totals = document.querySelectorAll(".total");
+  orderItem = document.querySelectorAll(".orderItem");
+  let pastEdit = document.querySelector(".edit");
+  if (newItem) {
+    newItem.classList.add("edit");
+    newItem.classList.remove("new");
+    if (pastEdit != null) {
+      pastEdit.classList.remove("edit");
+    }
+  }
+
+  item = "";
+  delButtons(delBtns, totals, qty, items, tBill, bill, footer, main);
+
+  qty = document.querySelectorAll(".qty");
+  qtyListener(qty, pastEdit);
+
+  items.innerHTML = qty.length;
+  tBill = 0;
+  totals.forEach((e) => {
+    tBill += Number(e.innerHTML);
+  });
+  bill.innerHTML = tBill;
+}
+if (document.querySelector(".menu1")) {
+  document.querySelector(".menu1").click();
+}
+addBtn.forEach((e) => {
+  e.addEventListener("click", (e) => {
+    changeLayout(1, "40%", "block", "0%", "none", "60%");
+    item = e.target.previousElementSibling;
+    name = item.children[1].children[0].innerHTML;
+    price = item.children[1].children[1].innerHTML;
+    price = price.split(".");
+    price[1] = pdCal(name, price[1]);
+    orderItem = document.querySelectorAll(".orderItem");
+    let match = false;
+    if (orderItem.length > 0) {
+      orderItem.forEach(e => {
+        if (name == e.children[0].children[0].innerHTML) {
+          let qty = e.children[1].children[0].children[0];
+          qty.innerHTML = (1 + Number(qty.innerHTML));
+          match = true;
+          updatePrice(qty);
+        }
+      });
+    }
+    if (!match) {
+      item = `<div class="orderItem orderItemAnimation">
+        <div class="orderName d-flex">
+            <h5>${name}</h5>
+            <img src="../../assets/deleteIcon.svg" alt="delete icon" class="delImg">
+        </div>
+        <div class="orderPrice d-flex">
+            <div class="count d-flex">
+                <p class="qty new">1</p>
+            </div>
+            <div class="calculation d-flex">
+                <p class="price">x ${price[1]}</p>
+                <p class="total">${price[1]}</p>
+            </div>
+        </div>
+    </div>`;
+      orderContainer.innerHTML += item;
+      match = false;
+    }
+    setTimeout(() => {
+      document.querySelectorAll(".orderItem").forEach((e) => {
+        e.classList.remove("orderItemAnimation");
+      });
+    }, 100);
+    qty = document.querySelector(".qty");
+    newItem = document.querySelector(".new");
+    delBtns = document.querySelectorAll(".delImg");
+    totals = document.querySelectorAll(".total");
+    orderItem = document.querySelectorAll(".orderItem");
+    let pastEdit = document.querySelector(".edit");
+    if (newItem) {
+      newItem.classList.add("edit");
+      newItem.classList.remove("new");
+      if (pastEdit != null) {
+        pastEdit.classList.remove("edit");
+      }
+    }
+    item = "";
+    delButtons(delBtns, totals, qty, items, tBill, bill, footer, main);
+    qty = document.querySelectorAll(".qty");
+    qtyListener(qty, pastEdit);
+    items.innerHTML = qty.length;
+    tBill = 0;
+    totals.forEach((e) => {
+      tBill += Number(e.innerHTML);
+    });
+    bill.innerHTML = tBill;
+  });
+
+});
+
+cancelBtn.addEventListener("click", () => {
+  orderContainer.innerHTML = "";
+  items.innerHTML = 0;
+  bill.innerHTML = 0;
+  changeLayout(0, "0%", "none", "10%", "block", "90%");
+  menuGrid = document.querySelector(".menuGrid");
+  menuGrid.classList.remove("col-3");
+});
+
+orderNowBtn.addEventListener("click", () => {
+  if (items.innerHTML != "0") {
+    orderNowBtnArr.forEach(e => {
+      display(e.vname, e.value, e.command, e.class);
+    });
+    body.classList.add("animation");
+    orderContainer.style.height = "66vh";
+    setTimeout(() => {
+      if (updateClicked) {
+        document.querySelector(".tables").click();
+      }
+    }, 0)
+
+    if (takeAway.classList.contains("active")) {
+      orderItem = document.querySelectorAll(".orderItem");
+      name = getOrderItem(orderItem, "takeAway");
+      orderArray.push({
+        description: name,
+        amount: Number(bill.innerHTML),
+      });
+      localStorage.setItem("takeAway", JSON.stringify(orderArray));
+      window.location.href = "../../index.html";
+    }
+    if (car.classList.contains("active")) {
+    }
+  }
+});
+menuIcon.addEventListener("click", () => {
+  changeLayout(0, "0%", "none", "10%", "block", "90%");
+})
+let array = [];
+let itemNames = "";
+let itemPrice = "";
+let itemqty = "";
+let amount = "";
+let totalAmount = "";
+
+
+function changeLayout(val1, val2, val3, val4, val5, val6) {
+  menuIcon.style.opacity = val1;
+  main.style.flexBasis = val6;
+  display(footer, val2, "flexBasis", val3);
+  display(header, val4, "flexBasis", val5);
+}
---
+++ pages/Order/script/Menu/orderSelection.js
@@ -0,0 +1,158 @@
+import { tables } from "../Data/tables.js";
+import { backBtnArr, assignTables, car, orderNowBtn, reserveTableBtn, tableOrder, carOrder, takeAway } from "../Component/reset.js";
+import { categoriesSelection, display, takeAwayfn, getOrderItem } from "../functions/function.js";
+import { bill } from "./order.js";
+let backBtn = document.querySelector(".backBtn");
+let input = document.querySelector(".form__field");
+let label = document.querySelector(".form__label"),
+    tableClick = sessionStorage.getItem("tableClick") ?
+        JSON.parse(sessionStorage.getItem("tableClick")) : "";
+const orderContainer = document.querySelector(".orderContainer");
+let allcircles = "", allTables = "", name = "", selectedTables = "", updateTable = "";
+let reserveOrderArray = localStorage.getItem('tableOrder')
+    ? JSON.parse(localStorage.getItem('tableOrder'))
+    : [];
+
+backBtn.addEventListener("click", (e) => {
+    backBtnArr.forEach((e) => {
+        display(e.vname, e.value, e.command, e.class);
+    });
+    document.querySelector(".menu1").click();
+    orderContainer.style.height = "55vh";
+});
+
+assignTables.addEventListener("click", (e) => {
+
+    categoriesSelection(car, carOrder);
+    takeAwayfn(takeAway);
+    assignTables.classList.add("active");
+    display(tableOrder, "grid", "add", "animation");
+    display(reserveTableBtn, "block", "add", "reserveActive");
+    orderNowBtn.style.display = "none";
+    let item = "";
+    tables.forEach((e) => {
+        if (!e.reserved) {
+            item += `<div class="d-flex table tableUnselected">
+                                <img src="../../assets/restaurant-table.png" alt="order">
+                                <h5>Table ${e.tableNo}</h5>
+                                <img src="../../assets/circle-w.png" alt="circle" class="circle">
+                            </div>`;
+        }
+        else {
+            item += `<div class="d-flex table tableSelected ">
+                        <img src="../../assets/restaurant-table.png" alt="order">
+                        <h5>Table ${e.tableNo}</h5>
+                        <img src="../../assets/circle-o.png" alt="circle" class="circle">
+                    </div>`;
+        }
+    });
+    tableOrder.innerHTML = item;
+    item = "";
+    allcircles = document.querySelectorAll(".circle");
+    allTables = document.querySelectorAll(".tableUnselected");
+    selectedTables = document.querySelectorAll(".tableSelected");
+    if (!tableClick.tableClicked) {
+        allcircles.forEach((e) => {
+            e.addEventListener("click", (e) => {
+                let img = e.target;
+                allTables.forEach((t) => {
+                    if (t.classList.contains("new")) {
+                        t.children[2].src = "../../assets/circle-w.png";
+                        t.classList.remove("new");
+                    }
+                    // console.log("inside");
+                });
+                img.src = "../../assets/circle-o.png";
+                img.parentNode.classList.add("new");
+                console.log(img.parentNode)
+                // console.log(allTables);
+                // console.log("clicked");
+            });
+        });
+    }
+    if (tableClick.tableClicked) {
+        reserveTableBtn.innerHTML = "Update";
+        updateTable = tableClick.tableNo;
+        selectedTables.forEach(t => {
+            let tableNum = t.children[1].innerHTML;
+            tableNum = Number(tableNum.substr(-2));
+            if (updateTable == tableNum) {
+                t.classList.add("update");
+                // t.children[0].src = "../assets/restaurant-table-update.png"
+            }
+        });
+    }
+
+});
+
+car.addEventListener("click", (e) => {
+    categoriesSelection(assignTables, tableOrder);
+    takeAwayfn(takeAway);
+    car.classList.add("active");
+    display(carOrder, "block", "add", "animation");
+    label.innerHTML = "Car Number";
+});
+
+takeAway.addEventListener("click", (e) => {
+    categoriesSelection(car, carOrder);
+    categoriesSelection(assignTables, tableOrder);
+    takeAway.classList.add("active");
+    reserveTableBtn.style.display = "none";
+    orderNowBtn.style.display = "block";
+});
+reserveTableBtn.addEventListener("click", (e) => {
+    // reserveOrderArray.forEach(e => {
+    //     if (e.tableNum == updateTable) { }
+    // })
+
+    allTables.forEach((t) => {
+        if (t.classList.contains("new")) {
+            let orderItem = document.querySelectorAll(".orderItem");
+            let i = t.children[1].innerHTML;
+            i = Number(i.substr(-2));
+            console.log(i);
+            tables[i - 1].reserved = true;
+            name = getOrderItem(orderItem, "reserved");
+            // console.log(name);
+            reserveOrderArray.push({
+                description: name,
+                total: Number(bill.innerHTML),
+                tableNum: i
+            });
+            // console.log(reserveOrderArray);
+            localStorage.setItem("tableOrder", JSON.stringify(reserveOrderArray));
+            localStorage.setItem("tables", JSON.stringify(tables));
+            // console.log(localStorage.getItem("tables"));
+            console.log(localStorage.getItem("tableOrder"));
+            window.location.href = "../../../index.html";
+        }
+    });
+    if (tableClick != "") {
+
+        // updateTable = Number(updateTable.slice(-1));
+        // console.log("🚀 ~ file: orderSelection.js ~ line 26 ~ assignTables.addEventListener ~ updateTable", updateTable);
+        let orderItem = document.querySelectorAll(".orderItem");
+        // tables[updateTable - 1].reserved = false;
+        name = getOrderItem(orderItem, "reserved");
+        console.log(reserveOrderArray);
+        // console.log("🚀 ~ file: orderSelection.js ~ line 119 ~ allTables.forEach ~ name", name);
+        reserveOrderArray.forEach(e => {
+            console.log(e.tableNum);
+            if (e.tableNum == updateTable) {
+                e.description = name;
+                e.total = Number(bill.innerHTML);
+                console.log("done");
+            }
+        });
+        console.log(reserveOrderArray);
+        localStorage.setItem("tableOrder", JSON.stringify(reserveOrderArray));
+        localStorage.setItem("tables", JSON.stringify(tables));
+        // console.log(localStorage.getItem("tables"));
+        console.log(localStorage.getItem("tableOrder"));
+        window.location.href = "../../../index.html";
+    }
+});
+
+export { allTables };
+// console.log(tables[0].reserved);
+// console.log(tables[0].tableNo);
