a8c165de94b58dd23fcabbab71b37c19759a8413
Mohtashim Ali <mohtashima85@gmail.com>
2021-10-30 12:00:31 +0500

    Update eventListeners.js

--- pages/Order/script/Data/eventListeners.js
+++ pages/Order/script/Data/eventListeners.js
@@ -3,6 +3,10 @@ export const menuOne = document.querySelector(".menu1"),
     menuTwo = document.querySelector(".menu2"),
     menuThree = document.querySelector(".menu3"),
     menuFour = document.querySelector(".menu4");
+export const amenuOne = document.querySelector(".amenu1"),
+    amenuTwo = document.querySelector(".amenu2"),
+    amenuThree = document.querySelector(".amenu3"),
+    amenuFour = document.querySelector(".amenu4");
 export let menuListenerArr = [
     {
         Vname: menuOne,
@@ -21,3 +25,22 @@ export let menuListenerArr = [
         args: [menuOne, "#menu1", menuTwo, "#menu2", menuThree, "#menu3", menuFour, "#menu4"]
     }
 ];
+
+export let adminMenuListenerArr = [
+    {
+        Vname: amenuOne,
+        args: [amenuTwo, "#adminMenu2", amenuThree, "#adminMenu3", amenuFour, "#adminMenu4", amenuOne, "#adminMenu1"]
+    },
+    {
+        Vname: amenuTwo,
+        args: [amenuOne, "#adminMenu1", amenuThree, "#adminMenu3", amenuFour, "#adminMenu4", amenuTwo, "#adminMenu2"]
+    },
+    {
+        Vname: amenuThree,
+        args: [amenuOne, "#adminMenu1", amenuTwo, "#adminMenu2", amenuFour, "#adminMenu4", amenuThree, "#adminMenu3"]
+    },
+    {
+        Vname: amenuFour,
+        args: [amenuOne, "#adminMenu1", amenuTwo, "#adminMenu2", amenuThree, "#adminMenu3", amenuFour, "#adminMenu4"]
+    }
+]
