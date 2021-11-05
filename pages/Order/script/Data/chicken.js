67ddec8927133cc866a5bbc0eb49950f4a121dad
Mohtashim Ali <mohtashima85@gmail.com>
2021-11-06 01:35:50 +0500

    Update chicken.js

--- pages/Order/script/Data/chicken.js
+++ pages/Order/script/Data/chicken.js
@@ -1,45 +1,54 @@
 export let chickenMenu = [
   {
+    id: 1,
     name: "Chicken Seekh Kabab",
     price: 800,
     type: "PD",
   },
   {
+    id: 2,
     name: "Chicken Boti",
     price: 800,
     type: "PD",
   },
   {
+    id: 3,
     name: "Chicken Malai Boti",
     price: 500,
     type: "PP",
   },
   {
+    id: 4,
     name: "Chicken Kabab Handi",
     price: 1100,
     type: "",
   },
   {
+    id: 5,
     name: "Chicken Handi",
     price: 1100,
     type: "F(Boneless)",
   },
   {
+    id: 6,
     name: "Chicken Handi",
     price: 300,
     type: "PP(Boneless)",
   },
   {
+    id: 7,
     name: "Chicken Karahi",
     price: 900,
     type: "F",
   },
   {
+    id: 8,
     name: "Chicken Karahi",
     price: 500,
     type: "H",
   },
   {
+    id: 9,
     name: "Chicken Steam Roast",
     price: 210,
     type: "Pc",
