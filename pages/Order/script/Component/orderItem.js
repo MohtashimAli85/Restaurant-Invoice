6cefb5ddc181a565f6ff155936578ee9a247cacb
Mohtashim Ali <mohtashima85@gmail.com>
2021-11-06 01:35:32 +0500

    Create orderItem.js

---
+++ pages/Order/script/Component/orderItem.js
@@ -0,0 +1,17 @@
+export function orderItemComponent(name, price, total) {
+    return `<div class="orderItem orderItemAnimation">
+        <div class="orderName d-flex">
+            <h5>${name}</h5>
+            <img src="../../assets/deleteIcon.svg" alt="delete icon" class="delImg">
+        </div>
+        <div class="orderPrice d-flex">
+            <div class="count d-flex">
+                <p class="qty new">1</p>
+            </div>
+            <div class="calculation d-flex">
+                <p class="price">x ${price}</p>
+                <p class="total">${total}</p>
+            </div>
+        </div>
+    </div>`;
+}
