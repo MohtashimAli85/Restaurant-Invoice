97a30d4fac04f64b41a199c5c7628cf0f84fec29
Mohtashim Ali <mohtashima85@gmail.com>
2022-01-20 22:53:39 +0500

    Update eventListeners.js

--- pages/Order/script/Data/eventListeners.js
+++ pages/Order/script/Data/eventListeners.js
@@ -7,6 +7,8 @@ export const amenuOne = document.querySelector(".amenu1"),
     amenuTwo = document.querySelector(".amenu2"),
     amenuThree = document.querySelector(".amenu3"),
     amenuFour = document.querySelector(".amenu4");
+export const addCrud = document.querySelector(".crud .add"), updateCrud = document.querySelector(".crud .update"),
+    delCrud = document.querySelector(".crud .delete");
 export let muttonDB = [], chickenDB = [], drinksDB = [], sideOrderDB = [];
 export let menuListenerArr = [
     {
@@ -38,18 +40,40 @@ export let menuListenerArr = [
 export let adminMenuListenerArr = [
     {
         Vname: amenuOne,
-        args: [amenuTwo, "#adminMenu2", amenuThree, "#adminMenu3", amenuFour, "#adminMenu4", amenuOne, "#adminMenu1"]
+        args: [amenuTwo, "#adminMenu2", amenuThree, "#adminMenu3", amenuFour, "#adminMenu4", amenuOne, "#adminMenu1"],
+        menuName: "mutton",
+        menuArr: muttonDB
     },
     {
         Vname: amenuTwo,
-        args: [amenuOne, "#adminMenu1", amenuThree, "#adminMenu3", amenuFour, "#adminMenu4", amenuTwo, "#adminMenu2"]
+        args: [amenuOne, "#adminMenu1", amenuThree, "#adminMenu3", amenuFour, "#adminMenu4", amenuTwo, "#adminMenu2"],
+        menuName: "chicken",
+        menuArr: chickenDB
     },
     {
         Vname: amenuThree,
-        args: [amenuOne, "#adminMenu1", amenuTwo, "#adminMenu2", amenuFour, "#adminMenu4", amenuThree, "#adminMenu3"]
+        args: [amenuOne, "#adminMenu1", amenuTwo, "#adminMenu2", amenuFour, "#adminMenu4", amenuThree, "#adminMenu3"],
+        menuName: "drinks",
+        menuArr: drinksDB
     },
     {
         Vname: amenuFour,
-        args: [amenuOne, "#adminMenu1", amenuTwo, "#adminMenu2", amenuThree, "#adminMenu3", amenuFour, "#adminMenu4"]
+        args: [amenuOne, "#adminMenu1", amenuTwo, "#adminMenu2", amenuThree, "#adminMenu3", amenuFour, "#adminMenu4"],
+        menuName: "sideOrder",
+        menuArr: sideOrderDB
+    }
+];
+export let adminCRUDList = [
+    {
+        Vname: addCrud,
+        args: [updateCrud, 'update', delCrud, 'delete', addCrud, 'add']
+    },
+    {
+        Vname: updateCrud,
+        args: [delCrud, 'delete', addCrud, 'add', updateCrud, 'update']
+    },
+    {
+        Vname: delCrud,
+        args: [updateCrud, 'update', addCrud, 'add', delCrud, 'delete']
     }
 ]
