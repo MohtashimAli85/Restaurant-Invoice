40e001217173897586fdb381d2f8744120e4bc6c
Mohtashim Ali <mohtashima85@gmail.com>
2022-01-20 22:53:46 +0500

    Create IndexedDBfn.js

---
+++ pages/Order/script/functions/IndexedDBfn.js
@@ -0,0 +1,103 @@
+import { fillDataArr } from '../Data/fillData.js';
+// import { fillingData } from './function.js';
+import { menuFn } from './menuFunctions.js';
+// import { read } from '../Menu/menu.js';
+export function testing101(arr, cb) {
+    arr.forEach(e => {
+        let mn = e.menuName, mr = e.menuArr;
+        e.Vname.addEventListener("click", () => {
+            menuFn(...e.args);
+            read(mn, mr, cb);
+        });
+    });
+}
+let db, request = window.indexedDB.open("AminKababHouse", 2);
+request.onsuccess = function (e) {
+    db = request.result;
+    console.log("success", db);
+    // if (JSON.parse(localStorage.getItem("DataBaseBuilt"))) {
+    //     document.querySelector(".menu1").click();
+
+    // }
+    // if (JSON.parse(localStorage.getItem('reloaded'))) {
+    //     localStorage.removeItem('reloaded');
+    //     window.location.href = "../../index.html";
+
+    // }
+};
+function read(menu, menuArr, callBack) {
+    try {
+        let objectStore = db.transaction(menu).objectStore(menu);
+        objectStore.openCursor().onsuccess = function (e) {
+            let cursor = (e.target.result);
+            if (cursor) {
+
+                if (menuArr.length > 0) {
+                    let matched = false;
+                    menuArr.forEach(e => {
+                        if (e.name == cursor.value.name && e.type == cursor.value.type) {
+                            // do nothing
+                            matched = true;
+                        }
+                    });
+                    if (!matched) {
+                        menuArr.push({
+                            name: cursor.value.name,
+                            price: cursor.value.price,
+                            type: cursor.value.type
+                        });
+                    }
+                } else {
+                    menuArr.push({
+                        name: cursor.value.name,
+                        price: cursor.value.price,
+                        type: cursor.value.type
+                    });
+                }
+                cursor.continue();
+            } else {
+                fillDataArr.forEach(e => {
+                    fillingData(menuArr, e.item, e.img, e.imgName, e.menu);
+                    e.item = "";
+                });
+                menuArr = [];
+                callBack();
+            }
+        }
+    } catch (e) {
+        console.log(e);
+        let deleteRequest = indexedDB.deleteDatabase('AminKababHouse');
+        console.log(deleteRequest);
+        deleteRequest.onsuccess = () => {
+            // location.reload();
+            localStorage.setItem('reloaded', JSON.stringify(true));
+        }
+        deleteRequest.onerror = () => {
+            // location.reload();
+            localStorage.setItem('reloaded', JSON.stringify(true));
+        }
+        deleteRequest.onblocked = () => {
+            // location.reload();
+            localStorage.setItem('reloaded', JSON.stringify(true));
+        }
+    }
+}
+function fillingData(Vname, item, img, imgName, menu) {
+    Vname.forEach(e => {
+        item += dataProvider(e.name, e.type, e.price, img, imgName);
+    });
+    menu.innerHTML = item;
+    item = "";
+}
+function dataProvider(name, type, price, img, imgName) {
+    return (`<div class="item d-flex">
+    <div class="d-flex v-center">
+        <img src="../../assets/${img}.png" alt="${imgName} img">
+        <div>
+        <h6>${name} <span>(${type})</span></h6>
+        <p>Rs.${price}</p>
+        </div>
+    </div>
+      <img src="../../assets/add-icon.svg" alt="add icon" class="addIcon addBtn">
+  </div>`)
+};
